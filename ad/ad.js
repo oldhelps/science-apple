bea650289ba975c8d51b68c5d1bef7ad71c73765
Oldhelps <99809158+oldhelps@users.noreply.github.com>
2022-10-30 16:33:57 +0800

    Create ad.js

---
+++ ad/ad.js
@@ -0,0 +1,11 @@
+$(function(){
+    // setInterval(()=>{
+    // },5000)
+    $('.adb').click(function (e) { 
+     window.location.href="https://oldhelps.github.io"
+    });
+    $("#c").click(function (e) { 
+       $(".ad").addClass("h");
+       console.log(e)
+    });
+})
